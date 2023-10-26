80dfd7ed4c0981561d8871394e0b7d03673efa31
Harry <harrytjbreen@gmail.com>
2023-10-26 16:41:00 +0100

    refactor: clean up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import {Authenticator} from "@aws-amplify/ui-react";
+import {withAuthenticator, WithAuthenticatorProps} from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react/styles.css';
 
 import './aws-exports'
 
-const App = () => {
-  return (
-      <Authenticator>
-          {({ signOut, user }) => {
-
-              console.log(user);
-
-              return <main>
-                  <h1>Hello {user!.username}</h1>
-                  <button onClick={signOut}>Sign out</button>
-              </main>
-          }}
-      </Authenticator>
-  );
+export function App({ signOut, user }: WithAuthenticatorProps) {
+    return (
+        <>
+            <h1>Hello {user?.username}</h1>
+            <button onClick={signOut}>Sign out</button>
+        </>
+    );
 }
 
-export default App;
+export default withAuthenticator(App);
